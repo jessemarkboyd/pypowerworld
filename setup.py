139a37ee84fda0399501850127081b6e4878fd80
jessemarkboyd <jessemarkboyd@gmail.com>
2017-01-06 12:15:18 -0800

    Change encoding to UTF-8

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+﻿from distutils.core import setup
 setup(
-    name = "pypowerworld",
-    author = "Jesse Boyd",
-    author_email = r"jessemarkboyd@gmail.com",
-    url = r"https://github.com/jessemarkboyd/pypowerworld",
-    download_url = r"https://github.com/jessemarkboyd/pypowerworld/tarball/0.1",
-    keywords = ["testing", "logging", "powerworld", "powerflow", "loadflow"],
-    version = "0.1.0",
-    description = "Powerworld COM wrapper for Python",
-    long_description = open("README.txt").read(),
-    packages = ["pypowerworld",],
-    license = "Creative Commons Attribution-Noncommercial-Share Alike license",
+    name = 'pypowerworld',
+    author = 'Jesse Boyd',
+    author_email = 'jessemarkboyd@gmail.com',
+    url = 'https://github.com/jessemarkboyd/pypowerworld',
+    download_url = 'https://github.com/jessemarkboyd/pypowerworld/tarball/0.1',
+    keywords = ['testing', 'logging', 'powerworld', 'powerflow', 'loadflow'],
+    version = '0.1.0',
+    description = 'Powerworld COM wrapper for Python',
+    long_description = open('README.txt').read(),
+    packages = ['pypowerworld',],
+    license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
 )
