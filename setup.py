b492a59a492f8c0da3cbd587f25b26eb6033b26e
jessemarkboyd <jessemarkboyd@gmail.com>
2017-01-06 12:39:01 -0800

    Version 2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url = 'https://github.com/jessemarkboyd/pypowerworld',
     download_url = 'https://github.com/jessemarkboyd/pypowerworld/tarball/0.1',
     keywords = ['testing', 'logging', 'powerworld', 'powerflow', 'loadflow'],
-    version = '0.1.0',
+    version = '0.2',
     description = 'Powerworld COM wrapper for Python',
     long_description = open('README.txt',encoding='utf8').read(),
     packages = ['pypowerworld',],
