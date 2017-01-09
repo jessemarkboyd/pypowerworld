f12c4a3c209c08a269629ef66a513e6b1bbc573e
jessemarkboyd <jessemarkboyd@gmail.com>
2017-01-08 20:40:32 -0800

    Refined error handling

--- pypowerworld/pypowerworld.py
+++ pypowerworld/pypowerworld.py
@@ -6,157 +6,146 @@
 # be changed ad hoc.                                                #
 #####################################################################
 
-import sys, os.path
+import os.path
 import win32com.client
 from win32com.client import VARIANT
 import pythoncom
-import logging
-import time
 
 class PowerWorld(object):
+    # Each PowerWorld class instance represents a COM object, PowerWorld.pw_com, initialized with a *.pwb file
 
-    # Initilized by a connection to the PowerWorld COM object and a PowerWorld file
     def __init__(self,fullfile_path=None):
-        
-        # Instantize a COM object for the PowerWorld application
         self.pw_com = win32com.client.Dispatch('pwrworld.SimulatorAuto') 
-        
-        # Check to make sure file path exists
         if fullfile_path == None:
-            fullfile_path = input("Please enter full pwb file path and name:")
+            fullfile_path = input('Please enter full pwb file path and name:')
         self.file_path = os.path.split(fullfile_path)[0]
         self.file_name = os.path.splitext(os.path.split(fullfile_path)[1])[0]
         self.auxfile_path = self.file_path + '/' + self.file_name + '.aux'
         self.output = ''
         self.error = False
-	self.error_message = ‘’
-	self.open_case()
-	if self.error:
-		print(“Error encountered opening file %s:/n/t%s” %(self.file_name,self.error_message))
-		print(“Please check the file name and path and try again (using the open_case method)”)
+        self.error_message = ''
+        self.open_case()
 
-
-    # Open *.pwb case
-    def open_case(self):
-        temp_status = self.set_output(self.pw_com.OpenCase(self.file_path + '/' + self.file_name + '.pwb'))
-        if not temp_status:
-            print("Error opening case")
-        return temp_status
-
-    # this method should be used to change output, otherwise errors may not be caught
-    def set_output(self, temp_output):
-        if temp_output[0] != '':
-            print(temp_output)
+    def __pw_err__(self, pw_output):
+        if pw_output is None:
             self.output = None
-            return False
-        elif "No data returned" in temp_output[0]:
+            self.error = False
+            self.error_message = ''
+        elif pw_output[0] != '':
             self.output = None
-            return True
+            self.error = False
+            self.error_message = ''
+        elif 'No data returned' in pw_output[0]:
+            self.output = None
+            self.error = False
+            self.error_message = pw_output[0]
         else:
-            self.output = temp_output
-            return True
+            self.output = pw_output
+            self.error = True
+            self.error_message = pw_output[0]
+        return self.error            
+
+    def open_case(self, file_name=None, file_path=None):
+        # Opens case defined by file_name and file_path; if these are undefined, opens by previous file path
+        if file_path is not None:
+            self.file_path = file_path
+        if file_name is not None:
+            self.file_name = file_name      
+        self.pw_com.OpenCase(self.file_path + '/' + self.file_name + '.pwb')
+        if self.__pw_err__():
+            print('Error opening case:/n/n%s', self.error_message)
+            print('/nPlease check the file name and path and try again (using the open_case method)')
 
     def save_case(self):
-        time.sleep(5)
-        return self.set_output(self.pw_com.SaveCase(self.file_path + '/' + self.file_name + '.pwb','PWB', 1))
+        # Saves case with changes to existing file name and path
+        self.pw_com.SaveCase(self.file_path + '/' + self.file_name + '.pwb','PWB', 1)
+        if self.__pw_err__():
+            print('Error saving case:/n/n%s', self.error_message)
+            print('/nCASE NOT SAVED!')
 
-    def close_case(self):
-        temp_status = self.set_output(self.pw_com.CloseCase())
-        if not temp_status:
-            print("Error closing case")
-        return temp_status
-        
-    def change_file_name(self,new_file_name):
-        self.file_name = new_file_name 
+    def save_case_as(self, file_name=None, file_path=None):
+        # If file name and path are specified, saves case as a new file. Overwrites any existing file with the same name and path
+        if file_path is not None:
+            self.file_path = file_path
+        if file_name is not None:
+            self.file_name = file_name
         self.auxfile_path = self.file_path + '/' + self.file_name + '.aux'
-        self.save_case() 
+        self.save_case()
+            
+    def close_case(self):
+        # Closes case without saving changes
+        self.pw_com.CloseCase()
+        if self.__pw_err__():
+            print('Error closing case:/n/n%s', self.error_message)
 
     def run_script(self,scriptCommand):
-        return self.set_output(self.pw_com.RunScriptCommand(scriptCommand))
+        # Input a script command as in an Auxiliary file SCRIPT{} statement or the PowerWorld Script command prompt
+        self.pw_com.RunScriptCommand(scriptCommand)
+        if self.__pw_err__():
+            print('Error encountered with script:/n/n%s', self.error_message)
 
-    def load_aux(self,auxText):
+    def load_auxiliary_file_text(self,aux_text):
+        # Creates and loads an Auxiliary file under the file_path and file_name.aux with the text specified in aux_text parameter
         auxfile_obj = open(self.auxfile_path, 'w')
-        auxfile_obj.writelines(auxText)
+        auxfile_obj.writelines(aux_text)
         auxfile_obj.close()
-        return self.set_output(self.pw_com.ProcessAuxFile(self.auxfile_path))
+        self.pw_com.ProcessAuxFile(self.auxfile_path)
+        if self.__pw_err__():
+            print('Error running auxiliary text:/n/n%s', self.error_message)
 
-    def get_parameters_single_element(self,element_type,field_list,valueList):
+    def get_parameters_single_element(self, element_type = 'BUS', field_list = ['BusName', 'BusNum'], value_list = [0, 1]):
+        # Retrieves parameter data accourding to the fields specified in field_list. value_list consists of identifying parameter values and zeroes and should be the same length as field_list
         field_array = VARIANT(pythoncom.VT_VARIANT | pythoncom.VT_ARRAY, field_list)
-        value_array = VARIANT(pythoncom.VT_VARIANT | pythoncom.VT_ARRAY, valueList) 
-        output = self.pw_com.GetParametersSingleElement(element_type, field_array, value_array)
-        return self.set_output(output)
+        value_array = VARIANT(pythoncom.VT_VARIANT | pythoncom.VT_ARRAY, value_list) 
+        self.pw_com.GetParametersSingleElement(element_type, field_array, value_array)
+        if self.__pw_err__():
+            print('Error retrieving single element parameters:/n/n%s', self.error_message)
+        elif self.error_message != '':
+            print(self.error_message)
+        else:
+            return self.output[1]
+        return None
 
-    def get_parameters_multiple_element(self,element_type,filter_name,field_list):
+    def get_parameters_multiple_element(self, element_type = 'BUS', filter_name = '', field_list = ['BusNum','BusName']):
         field_array = VARIANT(pythoncom.VT_VARIANT | pythoncom.VT_ARRAY, field_list)
-        self.set_output(self.pw_com.GetParametersMultipleElement(element_type, field_array, filter_name))
-        return self.output
+        self.pw_com.GetParametersMultipleElement(element_type, field_array, filter_name)
+        if self.__pw_err__():
+            print('Error retrieving single element parameters:/n/n%s', self.error_message)
+        elif self.error_message != '':
+            print(self.error_message)
+        else:
+            return self.output[1]
+        return None
 
-    def get_parameters_multiple_element_into_dict(self,element_type,field_list,filter_name=' ',field_key_cols = None,optional_key_function = None):
-        element_dict = dict()
-        if self.get_parameters_multiple_element(element_type,filter_name,field_list):
-            for n in range(0,len(self.output[1][0])):
-                d = dict()
-                for i in range(0,len(self.output[1])):
-                    d[field_list[i]] = self.output[1][i][n] 
-                if field_key_cols == None:
-                    key = n
-                elif type(field_key_cols) is int:
-                    if optional_key_function == None:
-                        value = self.output[1][field_key_cols][n]
-                        if value == '':
-                            continue
-                        else:
-                            key = value
-                    else:
-                        key = optional_key_function(self.output[1][field_key_cols][n])
-                elif type(field_key_cols) is tuple:
-                    key = ''
-                    for x in range(0,len(field_key_cols)):
-                        if x > 0:
-                            key += ' -- '
-                        key += self.output[1][int(field_key_cols[x])][n]
-                else:
-                    key = n
-
-                # Error handle the case of non-unique keys (keys are used as an ID in dictionaries and must be unique)
-                if key in element_dict.keys():
-                    print('Attempting to get elements into dict: the key values supplied were not unique to the elements')
-                    print('Duplicate key in elements: %s' %str(key))
-
-                else:
-                    # The element dictionary object has a key as defined above and the element as defined in the d object
-                    element_dict[key] = d
-
-                # Clean up
-                del d
-                d = None
-
-        # Assume any PW error indicates no objects were returned in the requested data
-        else: 
-            logging.warning("No elements obtained for dictionary")
-        
-        return element_dict
-
-    def get_3PB_fault(self,bus_num):
+    def get_3PB_fault_current(self, bus_num):
+        # Calculates the three phase fault; this can be done even with cases which only contain positive sequence impedances
         script_cmd = ('Fault([BUS {}], 3PB);\n'.format(bus_num))
         self.run_script(script_cmd)
+        if self.__pw_err__():
+            print('Error running 3PB fault:/n/n%s', self.error_message)
         field_list = ['BusNum','FaultCurMag']
-        if self.get_parameters_single_element('BUS',field_list,[bus_num,0]):
-            return float(self.output[1][1])
+        self.get_parameters_single_element('BUS', field_list, [bus_num, 0])
+        if self.__pw_err__():
+            print('Error retrieving fault current data:/n/n%s', self.error_message)
         else:
-            return None
-
-    def delete_ctgs(self):
-        script_cmd = 'DELETE(CONTINGENCY);'
-        self.run_script(script_cmd)
+            return float(self.output[1][1])
         return None
         
+    def create_filter(self, element_type='BUS',filter_type=''):
+        return None
+        
+    def delete_ctgs(self):
+        # Deletes all contingencies in the case
+        script_cmd = 'DELETE(CONTINGENCY);'
+        self.run_script(script_cmd)
+        
     def reopen_case(self):
+        # Closes the case without saving and reopens it (use this to discard changes)
         self.close_case()
         self.open_case()
-        return
 
     def exit(self):
+        # Clean up for the PowerWorld COM object
         self.close_case()
         del self.pw_com
         self.pw_com = None
