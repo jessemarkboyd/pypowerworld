b21c3366f123e9232ddd44652b1d7ea8ea85d3d0
jessemarkboyd <jessemarkboyd@gmail.com>
2018-01-04 15:42:57 -0800

    create mypackage folder

---
+++ mypackage/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 04 15:33:37 2018
+
+@author: jesse
+"""
+
+
+VERSION = (0, 3, 2)
+
+__version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-﻿from distutils.core import setup
-setup(
-    name = 'pypowerworld',
-    author = 'Jesse Boyd',
-    author_email = 'jessemarkboyd@gmail.com',
-    url = 'https://github.com/jessemarkboyd/pypowerworld',
-    download_url = 'https://github.com/jessemarkboyd/pypowerworld/tarball/0.3.1',
-    keywords = ['testing', 'logging', 'powerworld', 'powerflow', 'loadflow'],
-    version = '0.3.1',
-    description = 'Powerworld COM wrapper for Python',
-    long_description = open('README.txt').read(),
-    py_modules = ['pypowerworld'],
-    license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
-    install_requires = ['pandas','numpy','pypiwin32','pywin32'],
-)
-
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Note: To use the 'upload' functionality of this file, you must:
