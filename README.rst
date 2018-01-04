50d501cb4eced8bc171728374d6c51b66216fca8
jessemarkboyd <jessemarkboyd@gmail.com>
2018-01-04 15:21:33 -0800

    debug setup README issues

---
+++ README.rst
@@ -0,0 +1,32 @@
+pypowerworld
+
+==== OVERVIEW ====
+
+pypowerworld provides a class as a user-friendly wrapper for the PowerWorld COM object. It initializes a class object with a loadflow case. If the case fails to open, the program does not error out but prints an error and proceeds without a case. The ‘open_case’ method may be used to attempt to open the PowerWorld case again.
+
+==== REQUIREMENTS ====
+
+pypowerworld requires COM which means it must be run on a Windows operating system. It also requires the user to have PowerWorld and SimAuto licenses. Any PowerWorld tools which require additional license are also necessary.
+
+==== INPUT ====
+
+The inputs are labeled similarly to the PowerWorld Auxiliary File documentation. The inputs can either be method parameters or attributes used to set data or get data from the PowerWorld program.
+
+==== OUPUT ====
+
+Data output is sent in dataframe format and set to the ‘output’ attribute. This is true only when the get data method is run. For all other operations, this attribute will be set to None.
+
+==== ERROR HANDLING ====
+
+If an error occurs, the ‘error’ attribute is set to True and the ‘error_message’ attribute is set to an error string value explaining the error. If an error is not encountered, the ‘error’ attribute is set to False and the ‘error_message’ is set to an empty string.
+
+==== TYPICAL IMPLEMENTATION ====
+
+from pypowerworld import pypowerworld
+
+case_path = r‘somepathsomecase.pwb’ pw = pypowerworld(case_path) pw.solve()
+
+if pw.error:
+print(“Error encountered solving power flow: %s” % pw.error_message)
+else:
+print(“The case was solved using the Newton-Raphson method.”)
--- setup.py
+++ setup.py
@@ -4,12 +4,115 @@ setup(
     author = 'Jesse Boyd',
     author_email = 'jessemarkboyd@gmail.com',
     url = 'https://github.com/jessemarkboyd/pypowerworld',
-    download_url = 'https://github.com/jessemarkboyd/pypowerworld/tarball/0.3.0',
+    download_url = 'https://github.com/jessemarkboyd/pypowerworld/tarball/0.3.1',
     keywords = ['testing', 'logging', 'powerworld', 'powerflow', 'loadflow'],
-    version = '0.3.0',
+    version = '0.3.1',
     description = 'Powerworld COM wrapper for Python',
     long_description = open('README.txt').read(),
     py_modules = ['pypowerworld'],
     license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
     install_requires = ['pandas','numpy','pypiwin32','pywin32'],
+)
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'pypowerworld'
+DESCRIPTION = 'Powerworld COM wrapper for Python'
+URL = 'https://github.com/jessemarkboyd/pypowerworld'
+EMAIL = 'jessemarkboyd@gmail.com'
+AUTHOR = 'Jesse Boyd'
+REQUIRED = [
+    'pandas','numpy','pypiwin32','pywin32'
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
